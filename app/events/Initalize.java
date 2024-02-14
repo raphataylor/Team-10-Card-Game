1436baa3ecf6c756dd9bb82b3400adcf0cb0d1c6
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-02-14 14:09:00 +0000

    added drawcards method and added logic for drawing card at end of turn, on button click.

--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -17,38 +17,34 @@ import utils.UnitSummonTest;
  * Indicates that both the core game loop in the browser is starting, meaning
  * that it is ready to recieve commands from the back-end.
  * 
- * { 
- *   messageType = “initalize”
+ * {
+ * messageType = “initalize”
  * }
  * 
  * @author Dr. Richard McCreadie
  *
  */
-public class Initalize implements EventProcessor{
+public class Initalize implements EventProcessor {
 
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 		// hello this is a change
-		
+
 		gameState.gameInitalised = true;
-		
+
 		gameState.something = true;
-		//Game currentGame = new Game();
-		
+		// Game currentGame = new Game();
 
 		Game.createBoard(out);
-		
+		Game.createPlayerDeck(out, gameState);
 
 		// User 1 makes a change
-		//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
-		//Loaders_2024_Check.test(out);
-		
-		//replace this with the initialisation method from the game class
-		//UnitSummonTest.givePlayerCard(out);
-		gameState.player1.setPlayerDeck(OrderedCardLoader.getPlayer1Cards(1));
-		gameState.player1.drawCard(out);
+		// CommandDemo.executeDemo(out); // this executes the command demo, comment out
+		// this when implementing your solution
+		// Loaders_2024_Check.test(out);
+
+		// replace this with the initialisation method from the game class
+		// UnitSummonTest.givePlayerCard(out);
 	}
 
 }
-
-
