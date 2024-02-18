940a95352f435e74961e950d7f4e020318f15ac4
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-02-18 13:43:49 +0000

    logic for moving unit on click of a tile

--- app/structures/basic/Tile.java
+++ app/structures/basic/Tile.java
@@ -8,8 +8,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * A basic representation of a tile on the game board. Tiles have both a pixel position
- * and a grid position. Tiles also have a width and height in pixels and a series of urls
+ * A basic representation of a tile on the game board. Tiles have both a pixel
+ * position
+ * and a grid position. Tiles also have a width and height in pixels and a
+ * series of urls
  * that point to the different renderable textures that a tile might have.
  * 
  * @author Dr. Richard McCreadie
@@ -18,8 +20,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Tile {
 
 	@JsonIgnore
-	private static ObjectMapper mapper = new ObjectMapper(); // Jackson Java Object Serializer, is used to read java objects from a file
-	
+	private static ObjectMapper mapper = new ObjectMapper(); // Jackson Java Object Serializer, is used to read java
+																// objects from a file
+
 	List<String> tileTextures;
 	int xpos;
 	int ypos;
@@ -28,9 +31,11 @@ public class Tile {
 	int tilex;
 	int tiley;
 	Unit unit;
-	
-	public Tile() {}
-	
+	private boolean hasUnit = false;
+
+	public Tile() {
+	}
+
 	public Tile(String tileTexture, int xpos, int ypos, int width, int height, int tilex, int tiley) {
 		super();
 		tileTextures = new ArrayList<String>(1);
@@ -42,7 +47,7 @@ public class Tile {
 		this.tilex = tilex;
 		this.tiley = tiley;
 	}
-	
+
 	public Tile(List<String> tileTextures, int xpos, int ypos, int width, int height, int tilex, int tiley) {
 		super();
 		this.tileTextures = tileTextures;
@@ -53,76 +58,94 @@ public class Tile {
 		this.tilex = tilex;
 		this.tiley = tiley;
 	}
+
 	public List<String> getTileTextures() {
 		return tileTextures;
 	}
+
 	public void setTileTextures(List<String> tileTextures) {
 		this.tileTextures = tileTextures;
 	}
+
 	public int getXpos() {
 		return xpos;
 	}
+
 	public void setXpos(int xpos) {
 		this.xpos = xpos;
 	}
+
 	public int getYpos() {
 		return ypos;
 	}
+
 	public void setYpos(int ypos) {
 		this.ypos = ypos;
 	}
+
 	public int getWidth() {
 		return width;
 	}
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
+
 	public int getHeight() {
 		return height;
 	}
+
 	public void setHeight(int height) {
 		this.height = height;
 	}
+
 	public int getTilex() {
 		return tilex;
 	}
+
 	public void setTilex(int tilex) {
 		this.tilex = tilex;
 	}
+
 	public int getTiley() {
 		return tiley;
 	}
+
 	public void setTiley(int tiley) {
 		this.tiley = tiley;
 	}
-	
+
 	public void setUnit(Unit unit) {
 		this.unit = unit;
+		this.hasUnit = unit == null ? false : true;
 	}
-	
+
+	public boolean hasUnit() {
+		return this.hasUnit;
+	}
+
 	public Unit getUnit() {
 		return this.unit;
 	}
-	
+
 	/**
 	 * Loads a tile from a configuration file
 	 * parameters.
+	 * 
 	 * @param configFile
 	 * @return
 	 */
 	public static Tile constructTile(String configFile) {
-		
+
 		try {
 			Tile tile = mapper.readValue(new File(configFile), Tile.class);
 			return tile;
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
 		return null;
-		
+
 	}
-	
-	
-	
+
 }
