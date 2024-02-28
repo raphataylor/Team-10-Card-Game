18eb56d8357c273a5ececf830ae34cdab1bd9ec9
imgiri <2900778m@student.gla.ac.uk>
2024-02-28 03:58:35 +0000

    Spell interface and all the spell classes

---
+++ app/structures/spells/SundropElixir.java
@@ -0,0 +1,20 @@
+package structures.spells;
+
+import akka.actor.ActorRef;
+import structures.GameState;
+import structures.basic.Card;
+import structures.basic.Tile;
+
+/*
+ * Spell Name - Sundrop Elixir
+ * Player Deck - AI Player (Player 2)
+ * Cost = 1
+ * Effects: Heal allied unit for 4 health (this does not increase its maximum health)
+ * */
+
+public class SundropElixir extends Card implements Spell{
+	
+	public void spell(ActorRef out, GameState gameState, Tile tile){
+    }
+
+}
---
+++ app/structures/spells/WraithlingSwarm.java
@@ -0,0 +1,21 @@
+package structures.spells;
+
+import akka.actor.ActorRef;
+import structures.GameState;
+import structures.basic.Card;
+import structures.basic.Tile;
+
+/*
+ * Spell Name - Wraithling Swarm
+ * Player Deck - Human Player (Player 1)
+ * Cost = 3
+ * Effects:
+ 			Summon 3 Wraithlings in sequence.
+ * */
+
+public class WraithlingSwarm extends Card implements Spell{
+	
+	public void spell(ActorRef out, GameState gameState, Tile tile){
+    }
+
+}
